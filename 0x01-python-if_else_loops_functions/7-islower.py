9e21573f2f79f1c9d245b5845967419fc4382e93
adnan-jahbli <adnan.jahbli@gmail.com>
2023-06-07 01:42:38 +0000

    fixing pycodestyle

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if c == "":
-        c = ord(c) #this line was added just to generate error in this case
+        c = ord(c)  # this line was added just to generate error in this case
     LowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
     return c in LowercaseLetters
