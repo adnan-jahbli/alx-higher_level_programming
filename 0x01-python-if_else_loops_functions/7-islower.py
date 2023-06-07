2cf8e1bccda2091f4144b7f5920a7e83e39acc2d
adnan-jahbli <adnan.jahbli@gmail.com>
2023-06-07 01:40:49 +0000

    updating file 7'

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
+    if c == "":
+        c = ord(c) #this line was added just to generate error in this case
     LowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
-    return ord(c) in LowercaseLetters
+    return c in LowercaseLetters
