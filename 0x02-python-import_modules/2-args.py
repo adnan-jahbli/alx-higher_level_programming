f7d22366ef315241a3d6f21da7e7b0197d9bd19b
adnan-jahbli <adnan.jahbli@gmail.com>
2023-06-09 00:14:11 +0000

    updating file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,14 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     argv_len = len(argv) - 1
-    if argv_len == 1:
-        print("{} argument:".format(argv_len))
-    elif argv_len > 1:
-        print("{} arguments:".format(argv_len))
-    else:
+    if argv_len == 0:
         print("{} arguments.".format(argv_len))
-
-    i = 1
-    while i <= argv_len:
-        print("{}: {}".format(i, argv[i]))
-        i += 1
+    elif argv_len == 1:
+        print("{} argument:".format(argv_len))
+    else:
+        print("{} arguments:".format(argv_len))
+    if argv_len >= 1:
+        i = 1
+        while i <= argv_len:
+            print("{}: {}".format(i, argv[i]))
+            i += 1
