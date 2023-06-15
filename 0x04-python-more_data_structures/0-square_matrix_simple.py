d5adcf5540251f26e17ebd740fe69200a24c52b5
adnan-jahbli <adnan.jahbli@gmail.com>
2023-06-15 01:05:52 +0000

    creating file 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = [num ** 2 for num in matrix[i]]
+
+    return new_matrix
