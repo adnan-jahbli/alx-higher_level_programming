2ed99e647fd70c62b719141f4fb85e27594dd176
adnan-jahbli <adnan.jahbli@gmail.com>
2023-06-27 00:37:17 +0000

    fixing code style

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,4 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("out of range")
                 my_list_3[i] = 0
     finally:
-         return my_list_3
+        return my_list_3
