babeec86b2ed9bc0571f784d9b86132be2bf18aa
adnan-jahbli <adnan.jahbli@gmail.com>
2023-06-27 22:35:36 +0000

    creating file 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A square represented by a Square class"""
+
+
+class Square:
+    """Class represents a square"""
+
+    def __init__(self, size):
+        """ initializing the class
+
+        Args:
+            size (int): the size of the square
+        """
+        self.__size = size
