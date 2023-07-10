7fe6a9945c3518cc84cb41b5c95d7747e6eca59e
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-10 22:29:22 +0000

    updating file 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,6 @@ def inherits_from(obj, a_class):
     Returns:
         True or false.
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    if type(obj) is a_class:
+        return False
+    return isinstance(obj, a_class)
