07359f2586d4ccc2b4cc60b2bae16aec874aceb6
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-10 21:44:59 +0000

    updating file 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -16,7 +16,4 @@ def is_same_class(obj, a_class):
         specified class.
         False otherwise.
     """
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
