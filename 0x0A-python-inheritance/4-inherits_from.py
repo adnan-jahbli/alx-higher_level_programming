9203f4bbbb8c342183a3c84cdd7815821fca2620
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-10 22:31:38 +0000

    updating file 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-""" This module contains a function that checks if a given object
-is an instance of a given class that inherited (directly or indirectly)
-from the specified class """
-
-
 def inherits_from(obj, a_class):
     """ Checks if an object was instantiated from a given class
     that inherited directly or indirectly from the specified class.
