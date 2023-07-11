8cc5c84ec3de23676c6e79d8df27ec510b9cac17
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-11 22:24:57 +0000

    updating file 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 of text file (UTF8) and returns the number of characters added """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ This function appednds a string at the end of a text file (UTF8).
 
     Args:
