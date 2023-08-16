c451cb09b6f006cb330479efa0f88c87771927a3
adnan-jahbli <adnan.jahbli@gmail.com>
2023-08-16 00:20:44 +0000

    fixing syntax error in file 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- A script that inserts a new row in a table
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best Scool');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
